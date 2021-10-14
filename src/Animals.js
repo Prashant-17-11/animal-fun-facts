6b69dee7351a6ea702620640270db7732cefa447
Prashant-17-11 <prashantsingh.999000@gmail.com>
2021-10-14 12:15:14 +0530

    Completed the displayFacts funciton

---
+++ src/Animals.js
@@ -0,0 +1,18 @@
+import dolphinImage from "./images/dolphin.jpg";
+import lobsterImage from "./images/Lobster.jpg";
+import starfishImage from "./images/starfish.jpg";
+
+export const animals = {
+  dolphin: {
+    image: dolphinImage,
+    facts: ["some facts d", "some more facts d", "some thing more d"],
+  },
+  lobster: {
+    image: lobsterImage,
+    facts: ["some facts l", "some more facts l", "some thing more l"],
+  },
+  starfish: {
+    image: starfishImage,
+    facts: ["some facts s", "some more facts s", "some thing more s"],
+  },
+};
--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import backgroundImage from "./images/ocean-bed.jpg";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import { animals } from "./Animals";
+import "./App.css";
+// importing styling for app
+
+const title = "";
+// to store the title of the animal whose fact is being shown
+const images = [];
+// to store the images of animals
+const showBackground = true;
+// to store whether to show background or not
+const background = (
+  <img src={backgroundImage} className="background" alt="ocean" />
+);
+// to store the background for the app
+
+const animalFacts = (
+  <div>
+    <h1>{title === "" ? "Click an Animal for Fun Fact!!!" : title}</h1>{" "}
+    <p id="fact"></p>
+    {showBackground ? background : null}
+    <div className="animals">{images}</div>
+  </div>
+);
+// to store the facts to be shown
+// if there is no title then we show default message
+
+for (const animal in animals) {
+  images.push(
+    <img
+      key={animal}
+      className="animal"
+      src={animals[animal].image}
+      alt={animal}
+      aria-label={animal}
+      role="button"
+      onClick={displayFacts}
+    />
   );
 }
+// creating an array of images for each animal
+
+function displayFacts(e) {
+  // e means event
+  const facts = animals[e.target.alt].facts;
+  //console.log(facts);
+  //console.log(facts.length);
+  const randomNum = Math.floor(Math.random() * facts.length);
+  //console.log(randomNum);
+  const fact = facts[randomNum];
+  //console.log(fact);
+  document.getElementById("fact").innerHTML = fact;
+}
+// this function will display facts
+// console's were for debugging
+
+function App() {
+  return animalFacts;
+} // sending our animal Facts JSX to be rendered in index.js
 
 export default App;
