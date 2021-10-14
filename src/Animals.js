0c84e61374713610ba0b03178050f565977ecc96
Prashant-17-11 <prashantsingh.999000@gmail.com>
2021-10-14 12:39:22 +0530

    updated the images and added some facts

--- src/Animals.js
+++ src/Animals.js
@@ -5,14 +5,39 @@ import starfishImage from "./images/starfish.jpg";
 export const animals = {
   dolphin: {
     image: dolphinImage,
-    facts: ["some facts d", "some more facts d", "some thing more d"],
+    facts: [
+      "They stick with their mothers for a long time",
+      "Dolphins have 2 stomachs",
+      "Dolphins can live up to 50 years!",
+      "There are around 40 species of dolphins",
+      "Dolphins help the sick or injured members",
+      "Dolphins never chew their food",
+      "They give themselves names",
+      "Dolphins are very friendly to humans and animals",
+      "Killer whales are the biggest kind of dolphin",
+      "They have delicate skin but heals quickly",
+    ],
   },
   lobster: {
     image: lobsterImage,
-    facts: ["some facts l", "some more facts l", "some thing more l"],
+    facts: [
+      "They keep growing forever",
+      "They eat each other",
+      "They chew with their stomachs",
+      "They taste with their legs",
+      " One of their claws can exert pressure of up to 100 pounds per square inch",
+      "They can regenerate limbs",
+    ],
   },
   starfish: {
     image: starfishImage,
-    facts: ["some facts s", "some more facts s", "some thing more s"],
+    facts: [
+      "Starfish can regenerate their own arms ",
+      "They have no brain or blood!",
+      "Starfish have eyes",
+      "Starfish move with hundreds of feet",
+      "Starfish can eat outside their body",
+      "Not all starfish are star shaped",
+    ],
   },
 };
